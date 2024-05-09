63cc43ab4802f76d90a62ea759e5d762d6776e18
Petter Olsson <petter@dominodatalab.com>
2024-05-09 04:57:04 -0700

    Pushed from Domino: https://demo.eval.domino.tech/workspace/petter/MLOps-Best-Practices?executionId=6630b81e8fc75734b7f7b412

--- scripts/sklearn_model_train.py
+++ scripts/sklearn_model_train.py
@@ -13,11 +13,7 @@ import mlflow
 import mlflow.sklearn
 from mlflow.store.artifact.runs_artifact_repo import RunsArtifactRepository
 from mlflow import MlflowClient
-from domino_data_capture.data_capture_client import DataCaptureClient
-import uuid
-import datetime
-import pickle
-
+ 
 #Read in data
 path = str('/mnt/data/{}/WineQualityData.csv'.format(os.environ.get('DOMINO_PROJECT_NAME')))
 df = pd.read_csv(path)
@@ -25,39 +21,39 @@ print('Read in {} rows of data'.format(df.shape[0]))
 
 #rename columns to remove spaces
 for col in df.columns:
-    df.rename({col: col.replace(' ', '_')}, axis=1, inplace=True)
+    df.rename({col: col.replace(' ', '_')}, axis =1, inplace = True)
 
 #Create is_red variable to store red/white variety as int    
 df['is_red'] = df.type.apply(lambda x : int(x=='red'))
 
 #Find all pearson correlations of numerical variables with quality
-corr_values = df.corr(numeric_only=True).sort_values(by='quality')['quality'].drop('quality', axis=0)
+corr_values = df.corr(numeric_only=True).sort_values(by = 'quality')['quality'].drop('quality',axis=0)
 
 #Keep all variables with above a 8% pearson correlation
-important_feats = corr_values[abs(corr_values) > 0.08]
+important_feats=corr_values[abs(corr_values)>0.08]
 
 #Get data set up for model training and evaluation
 
 #Drop NA rows
-df = df.dropna(how='any', axis=0)
+df = df.dropna(how='any',axis=0)
 #Split df into inputs and target
 X = df[important_feats.keys()]
 y = df['quality'].astype('float64')
 
-# create a new MLFlow experiment
+# create a new MLFlow experiemnt
 mlflow.set_experiment(experiment_name=os.environ.get('DOMINO_PROJECT_NAME') + " " + os.environ.get('DOMINO_STARTING_USERNAME'))
 
 with mlflow.start_run():
-    # Set MLFlow tag to differentiate the model approaches
+    # Set MLFlow tag to differenciate the model approaches
     mlflow.set_tag("Model_Type", "sklearn")
     mlflow.sklearn.autolog(silent=True)
     #Create 70/30 train test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-    #Initiate and fit Gradient Boosted Classifier
+    #initiate and fit Gradient Boosted Classifier
     print('Training model...')
-    gbr = GradientBoostingRegressor(loss='squared_error', learning_rate=0.15, n_estimators=75, criterion='squared_error')
-    gbr.fit(X_train, y_train)
+    gbr = GradientBoostingRegressor(loss='squared_error',learning_rate = 0.15, n_estimators=75, criterion = 'squared_error')
+    gbr.fit(X_train,y_train)
 
     #Predict test set
     print('Evaluating model on test data...')
@@ -65,37 +61,37 @@ with mlflow.start_run():
     autolog_run = mlflow.last_active_run()
 
     #View performance metrics and save them to domino stats!
-    print("R2 Score: ", round(r2_score(y_test, preds), 3))
-    print("Squared Error: ", round(mean_squared_error(y_test, preds), 3))
+    print("R2 Score: ", round(r2_score(y_test, preds),3))
+    print("Squared Error: ", round(mean_squared_error(y_test, preds),3))
     
     # Save the metrics in MLFlow
-    mlflow.log_metric("R2", round(r2_score(y_test, preds), 3))
-    mlflow.log_metric("Squared Error", round(mean_squared_error(y_test, preds), 3))
+    mlflow.log_metric("R2", round(r2_score(y_test, preds),3))
+    mlflow.log_metric("Squared Error", round(mean_squared_error(y_test,preds),3))
 
-    # Code to write R2 value and Squared Error to dominostats value for population in experiment manager
+    #Code to write R2 value and Squared Error to dominostats value for population in experiment manager
     with open('/mnt/artifacts/dominostats.json', 'w') as f:
-        f.write(json.dumps({"R2": round(r2_score(y_test, preds), 3),
-                           "Squared Error": round(mean_squared_error(y_test, preds), 3)}))
+        f.write(json.dumps({"R2": round(r2_score(y_test, preds),3),
+                           "Squared Error": round(mean_squared_error(y_test,preds),3)}))
 
     #Write results to dataframe for visualizations
-    results = pd.DataFrame({'Actuals': y_test, 'Predictions': preds})
+    results = pd.DataFrame({'Actuals':y_test, 'Predictions':preds})
 
     print('Creating visualizations...')
     #Add visualizations and save for inspection
-    fig1, ax1 = plt.subplots(figsize=(10, 6))
+    fig1, ax1 = plt.subplots(figsize=(10,6))
     plt.title('Sklearn Actuals vs Predictions Scatter Plot')
     sns.regplot( 
         data=results,
-        x='Actuals',
-        y='Predictions',
-        order=3)
+        x = 'Actuals',
+        y = 'Predictions',
+        order = 3)
     plt.savefig('/mnt/artifacts/actual_v_pred_scatter.png')
     mlflow.log_figure(fig1, 'actual_v_pred_scatter.png')
 
-    fig2, ax2 = plt.subplots(figsize=(10, 6))
+    fig2, ax2 = plt.subplots(figsize=(10,6))
     plt.title('Sklearn Actuals vs Predictions Histogram')
     plt.xlabel('Quality')
-    sns.histplot(results, bins=6, multiple='dodge', palette='coolwarm')
+    sns.histplot(results, bins=6, multiple = 'dodge', palette = 'coolwarm')
     plt.savefig('/mnt/artifacts/actual_v_pred_hist.png')
     mlflow.log_figure(fig2, 'actual_v_pred_hist.png')
 
@@ -108,40 +104,11 @@ with mlflow.start_run():
     # model_name = "WineQualityGradientBoostingRegressor"
     # mlflow.register_model(model_uri, model_name)
     
-    # Define Data Capture Client
-    feature_names = list(X.columns)
-    target_name = ['quality']
-    data_capture_client = DataCaptureClient(feature_names, target_name)
-
-    # Create a custom MLflow model class
-    class WineQualityModel(mlflow.pyfunc.PythonModel):
-        def __init__(self, model):
-            self.model = model
-        
-        def predict(self, context, model_input, params=None):
-            event_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
-            prediction = self.model.predict(model_input)
-            
-            if isinstance(model_input, pd.DataFrame):
-                model_input = model_input.values.tolist()
-            
-            for i in range(len(prediction)):
-                # Record eventID and current time
-                event_id = uuid.uuid4()
-                model_input_value = model_input[i]
-                prediction_value = [prediction[i]]
-                
-                # Capture this prediction event so Domino can keep track
-                data_capture_client.capturePrediction(model_input_value, prediction_value, event_id=event_id,
-                                    timestamp=event_time)
-            return prediction
-
-    # Wrap the model
-    wrapped_model = WineQualityModel(gbr)
-
 mlflow.end_run()
 
 #Saving trained model to serialized pickle object 
+import pickle 
+
 # save best model
 file = '/mnt/artifacts/models/sklearn_gbm.pkl'
 pickle.dump(gbr, open(file, 'wb'))
