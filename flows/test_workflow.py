f8c1876f285a5f215f657913c6e2fae4253e0b25
Petter Olsson <petter@dominodatalab.com>
2024-07-16 07:17:11 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/petter/MLOps-Best-Practices?executionId=6696629c044c5568b6892403

--- flows/test_workflow.py
+++ flows/test_workflow.py
@@ -6,7 +6,7 @@ def my_simple_test() -> str:
 
     # TASK ONE
     task_one = DominoJobTask(
-        name = "List files in directory",
+        name="List files in directory",
         domino_job_config=DominoJobConfig(Command="ls -ltrash"),
         inputs={},
         outputs={'output': str},
