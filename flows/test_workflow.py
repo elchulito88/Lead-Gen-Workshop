f9b232c39f07df3fc4cf9ee260232d40c5d04cd8
Petter Olsson <petter@dominodatalab.com>
2024-07-30 02:38:00 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/petter/MLOps-Best-Practices?executionId=6696629c044c5568b6892403

--- flows/test_workflow.py
+++ flows/test_workflow.py
@@ -7,7 +7,7 @@ def my_simple_test() -> str:
     # TASK ONE
     task_one = DominoJobTask(
         name="A small Hello",
-        domino_job_config=DominoJobConfig(Command="python", "flows/print_hello.py"),
+        domino_job_config=DominoJobConfig[Command="python", "flows/print_hello.py"],
         inputs={},
         outputs={'output': str},
         use_latest=True
