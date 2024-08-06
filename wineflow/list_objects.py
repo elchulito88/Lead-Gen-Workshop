f5800388d754b73386dfc6e63e2429a0e18660b2
Petter Olsson <petter@dominodatalab.com>
2024-08-06 07:48:01 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/petter/MLOps-Best-Practices?executionId=66aa193b90494e1dbaa23cd4

--- wineflow/list_objects.py
+++ wineflow/list_objects.py
@@ -2,7 +2,7 @@ from contextlib import redirect_stdout
 from domino.data_sources import DataSourceClient
 
 # Define the file path where you want to redirect the output
-file_path = "/mnt/artifacts/objects" #"/workflow/outputs/output"
+file_path = "/workflow/outputs/objects"
 
 # Open the file in write mode and use redirect_stdout to redirect print output
 with open(file_path, 'w') as f:
