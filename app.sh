20b01d2decbc1e4d4fd817b05517721f3c56ac53
Petter Olsson <petter@dominodatalab.com>
2024-11-15 02:36:28 -0800

    Pushed from Domino: https://ykb.domino-eval.com/workspace/petter/mlops-best-practices?executionId=6734940e0b77b232dfd8c697

--- app.sh
+++ app.sh
@@ -5,7 +5,7 @@
  
 ## R/Shiny Example
 ## This is an example of the code you would need in this bash script for a R/Shiny app
-R -e 'shiny::runApp("./apps/wine.R", port=8888, host="0.0.0.0")'
+R -e 'shiny::runApp("./apps/lead_gen.R", port=8888, host="0.0.0.0")'
  
 ## Flask example
 ## This is an example of the code you would need in this bash script for a Python/Flask app
