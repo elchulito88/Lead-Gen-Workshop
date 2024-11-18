e0ac030bff413240633f4490d2e675eebd1ebb4c
Petter Olsson <petter@dominodatalab.com>
2024-11-18 08:39:49 -0800

    fixed for credit approval workshop
    Pushed from Domino: https://ykb.domino-eval.com/workspace/petter/mlops-best-practices?executionId=6734940e0b77b232dfd8c697

--- scripts/multitrain.py
+++ scripts/multitrain.py
@@ -2,27 +2,51 @@ import domino
 import os
 import time
 
-# In this project we will use Domino's platform API through it's Python wrapper to kick off several training jobs at the same time!
+# Import mlflow & initiate client
+import mlflow
+import mlflow.sklearn
+import mlflow.tracking
+import mlflow.projects
 
+print('Initializing MLFlow Client')
+
+# Initiate MLFlow client
+client = mlflow.tracking.MlflowClient()
+
+# Verify MLFLow URI
+print('MLFLOW_TRACKING_URI: ' + os.environ['MLFLOW_TRACKING_URI'])
+
+experiment_name = os.environ.get('DOMINO_PROJECT_NAME') + " " + os.environ.get('DOMINO_STARTING_USERNAME')
+experiment = mlflow.set_experiment(experiment_name=experiment_name) #changeit
+
+print('Experiment "{}" created, Experiment ID: {}'.format(experiment_name, experiment.experiment_id))
+print("Artifact Location: {}".format(experiment.artifact_location))
+print("Tags: {}".format(experiment.tags))
+ 
 print('Initializing Domino Project for API calls')
 
-# Initialize Domino Project
-# Domino automatically passes down environment variables about the project, owner and even our individual API key and Token file for executing the Jobs in Domino.
+# Create the output image directory in a Domino Dataset, if it doesn't already exist
+path = '/domino/datasets/local/{}/visualizations'.format(os.environ.get('DOMINO_PROJECT_NAME'))
+
+if not os.path.exists(path):
+    os.makedirs(path)
+ 
+#initialize Domino Project
 domino_project =domino.Domino(project = str(os.environ.get('DOMINO_PROJECT_OWNER')+'/'+os.environ.get('DOMINO_PROJECT_NAME')),
-                              api_key = os.environ.get('DOMINO_USER_API_KEY'),
                               domino_token_file=os.environ.get('DOMINO_TOKEN_FILE'))
-
-
-# First we will start our sklearn model training 
-print('Kicking off sklearn model training')
-domino_project.job_start(command='scripts/sklearn_model_train.py')
-
-# Then our xgboost written in R
-print('Kicking off R model training')
-domino_project.job_start(command='scripts/R_model_train.R')
-
-# And finally our h2o automl model training
+ 
+print('Kicking off sklearn logisitc regression model training')
+domino_project.job_start(command='scripts/sklearn_log_reg_train.py')
+ 
 print('Kicking off h2o model training')
 domino_project.job_start(command='scripts/h2o_model_train.py')
+  
+print('Kicking off sklearn random forest model training')
+domino_project.job_start(command='scripts/sklearn_RF_train.py')
+
+print('Kicking off XGBoost model training')
+domino_project.job_start(command='scripts/xgb_model_train.py')
+
+
 
 print('Done!')
