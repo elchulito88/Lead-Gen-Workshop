5ff76b1cbb9296e7cdbc4b6e53617f184bd9835e
Bryan Prosser <bryan.prosser@dominodatalab.com>
2024-11-18 09:01:26 -0800

    Pushed from Domino: https://ykb.domino-eval.com/workspace/petter/mlops-best-practices?executionId=673b6ddd011f5130a7ff00d2

--- scripts/multitrain.py
+++ scripts/multitrain.py
@@ -8,28 +8,6 @@ import mlflow.sklearn
 import mlflow.tracking
 import mlflow.projects
 
-print('Initializing MLFlow Client')
-
-# Initiate MLFlow client
-client = mlflow.tracking.MlflowClient()
-
-# Verify MLFLow URI
-print('MLFLOW_TRACKING_URI: ' + os.environ['MLFLOW_TRACKING_URI'])
-
-experiment_name = os.environ.get('DOMINO_PROJECT_NAME') + " " + os.environ.get('DOMINO_STARTING_USERNAME')
-experiment = mlflow.set_experiment(experiment_name=experiment_name) #changeit
-
-print('Experiment "{}" created, Experiment ID: {}'.format(experiment_name, experiment.experiment_id))
-print("Artifact Location: {}".format(experiment.artifact_location))
-print("Tags: {}".format(experiment.tags))
- 
-print('Initializing Domino Project for API calls')
-
-# Create the output image directory in a Domino Dataset, if it doesn't already exist
-path = '/domino/datasets/local/{}/visualizations'.format(os.environ.get('DOMINO_PROJECT_NAME'))
-
-if not os.path.exists(path):
-    os.makedirs(path)
  
 #initialize Domino Project
 domino_project =domino.Domino(project = str(os.environ.get('DOMINO_PROJECT_OWNER')+'/'+os.environ.get('DOMINO_PROJECT_NAME')),
